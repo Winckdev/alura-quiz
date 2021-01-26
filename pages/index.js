3c64865c914f2c2f65d43124eb69feabcba7eae6
Arthur W <apwinck@gmail.com>
2021-01-26 11:51:42 -0300

    primeira aula imersao

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,47 @@
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget/index.js'
+import Footer from '../src/components/Footer/index.js'
+import GitHubCorner from '../src/components/GitHubCorner/index.js'
+import QuizBackground from '../src/components/QuizBackground/index.js'
+import QuizContainer from '../src/components/QuizContainer/index.js'
 
-const Title = styled.h1`
+/* const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+  color: ${({ theme }) => theme.colors.secondary};
+` */
+
+/* const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+` */
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Teste</h1>
+          </Widget.Header>
+          <Widget.Content>
+          <h1>lorem ipsum xesque dele dale dolly</h1>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Teste</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <h1>lorem ipsum xesque dele dale dolly</h1>
+          </Widget.Content>
+        </Widget>
+        <Footer/>
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/winckdev"/>
+    </QuizBackground>
+    
+  )
 }
